69414483c5842fee8fccc267c0f278b34657e923
FARAHALHAYAT <94931048+FARAHALHAYAT@users.noreply.github.com>
2022-07-29 23:19:25 +0400

    Update ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,4 +1,43 @@
-/* ************************************************************************** */
+int		ft_countsize(long int n);
+void	ft_convbase(long int n, char *number, long int i);
+
+char	*ft_itoa(int n)
+{
+	char		*number;
+	long int	len;
+
+	len = ft_countsize(n);
+	number = (char *)malloc((len + 1) * sizeof(char));
+	if (!number)
+		return (NULL);
+	number[len--] = '\0';
+	ft_convbase(n, number, len);
+	return (number);
+}
+
+// recursively count integer size
+int	ft_countsize(long int n)
+{
+	if (n < 0)
+		return (1 + ft_countsize(-n));
+	if ((n / 10) == 0)
+		return (1);
+	else
+		return (1 + ft_countsize(n / 10));
+}
+
+// recursively convert integer to string
+void	ft_convbase(long int n, char *number, long int i)
+{
+	if (n < 0)
+	{
+		number[0] = '-';
+		n *= -1;
+	}
+	if (n >= 10)
+		ft_convbase((n / 10), number, (i - 1));
+	number[i] = (n % 10) + '0';
+}/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
@@ -12,27 +51,109 @@
 
 #include "libft.h"
 
+int		ft_countsize(long int n);
+void	ft_convbase(long int n, char *number, long int i);
+
 char	*ft_itoa(int n)
 {
-	char	*str;
+	char		*number;
+	long int	len;
 
-	str = (char *)malloc(sizeof(char) * 2);
-	if (!str)
+	len = ft_countsize(n);
+	number = (char *)malloc((len + 1) * sizeof(char));
+	if (!number)
 		return (NULL);
-	if (n == -2147483648)
-		return ("-2147483648");
+	number[len--] = '\0';
+	ft_convbase(n, number, len);
+	return (number);
+}
+
+// recursively count integer size
+int	ft_countsize(long int n)
+{
+	if (n < 0)
+		return (1 + ft_countsize(-n));
+	if ((n / 10) == 0)
+		return (1);
+	else
+		return (1 + ft_countsize(n / 10));
+}
+
+// recursively convert integer to string
+void	ft_convbase(long int n, char *number, long int i)
+{
 	if (n < 0)
 	{
-		str[0] = '-';
-		str[1] = '\0';
-		str = ft_strjoin(str, ft_itoa(-n));
+		number[0] = '-';
+		n *= -1;
 	}
-	else if (n >= 10)
-		str = ft_strjoin(ft_itoa(n / 10), ft_itoa(n % 10));
-	else if (n < 10 && n >= 0)
+	if (n >= 10)
+		ft_convbase((n / 10), number, (i - 1));
+	number[i] = (n % 10) + '0';
+}
+
+		NEW
+static char	spe_putnbr(int nb)
+{
+	unsigned int	number;
+	number = nb;
+	if (number == 0)
+		return (0 + '0');
+	if (number > 0)
 	{
-		str[0] = n + '0';
-		str[1] = '\0';
+		spe_putnbr(number / 10);
+		number %= 10;
 	}
-	return (str);
+	return (number + '0');
+}
+static int	is_neg(int nb)
+{
+	if (nb < 0)
+		return (1);
+	return (0);
+}
+static size_t	get_len(int n)
+{
+	size_t			len;
+	unsigned int	number;
+	len = 0;
+	if (is_neg(n))
+	{
+		number = (unsigned int)-n;
+		len++;
+	}
+	else
+		number = (unsigned int)n;
+	while (number > 9)
+	{
+		number /= 10;
+		len++;
+	}
+	len++;
+	return (len);
+}
+char	*ft_itoa(int n)
+{
+	char			*result;
+	size_t			len;
+	unsigned int	num;
+	int				i;
+	len = get_len(n);
+	result = malloc(sizeof(char) * (len + 1));
+	if (result == NULL)
+		return (NULL);
+	i = 0;
+	if (is_neg(n))
+		num = (unsigned int)-n;
+	else
+		num = (unsigned int)n;
+	while (i++ <= (int)len - 1)
+	{
+		result[(int)len - i] = spe_putnbr(num);
+		num /= 10;
+	}
+	if (is_neg(n))
+		result[0] = '-';
+	result[len] = '\0';
+	return (result);
 }
