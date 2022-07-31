d6f061704ce16ba504c74411835d7d6a28607349
FARAHALHAYAT <94931048+FARAHALHAYAT@users.noreply.github.com>
2022-07-31 21:46:20 +0400

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -14,25 +14,16 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char			*str;
-	unsigned int	i;
+	char	*new_str;
+	size_t	i;
+	size_t	j;
 
-	i = 0;
-	if (s == NULL)
-		return (NULL);
-	if (len > ft_strlen(s) - start)
-			len = ft_strlen(s) - start;
-	if (start >= ft_strlen(s))
-		return (NULL);
-	str = (char *)malloc(sizeof(char) * (len + 1));
-	if (!str)
-		return (str);
-	while (i < len)
-	{
-		str[i] = s[start];
-		i++;
-		start++;
-	}
-	str[i] = '\0';
-	return (str);
+	if (!s || !(new_str = (char *)malloc(len + 1)))
+		return (0);
+	i = start;
+	j = 0;
+	while (i < ft_strlen(s) && j < len)
+		new_str[j++] = s[i++];
+	new_str[j] = '\0';
+	return (new_str);
 }
