cc1292c1e07ecd7b6492c5b161446b036089ea19
FARAHALHAYAT <94931048+FARAHALHAYAT@users.noreply.github.com>
2022-08-01 19:12:37 +0400

    Update ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -17,10 +17,10 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	unsigned int	i;
 	char			*str;
 
+	if (!(str = ft_strdup(s)))
+		return (NULL);
 	i = 0;
 	str = (char *)malloc(sizeof(char) * (ft_strlen(s)) + 1);
-	if (str == NULL)
-		return (NULL);
 	while (s[i] != '\0')
 	{
 		str[i] = f(i, s[i]);
