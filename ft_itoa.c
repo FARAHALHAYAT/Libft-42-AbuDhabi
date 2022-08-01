6cebdad57c11560190c004d052a64f7a6025ebbb
FARAHALHAYAT <94931048+FARAHALHAYAT@users.noreply.github.com>
2022-08-01 21:32:44 +0400

    Update ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -12,27 +12,48 @@
 
 #include "libft.h"
 
-char	*ft_itoa(int n)
+int			ft_len(long n)
 {
-	char	*str;
+	int	len;
 
-	str = (char *)malloc(sizeof(char) * 2);
-	if (!str)
-		return (NULL);
-	if (n == -2147483648)
-		return ("-2147483648");
+	len = 0;
 	if (n < 0)
 	{
-		str[0] = '-';
-		str[1] = '\0';
-		str = ft_strjoin(str, ft_itoa(-n));
+		n *= -1;
+		len++;
 	}
-	else if (n >= 10)
-		str = ft_strjoin(ft_itoa(n / 10), ft_itoa(n % 10));
-	else if (n < 10 && n >= 0)
+	while (n > 0)
 	{
-		str[0] = n + '0';
+		n = n / 10;
+		len++;
 	}
-	str[1] = '\0';
+	return (len);
+}
+
+char		*ft_itoa(int n)
+{
+	char	*str;
+	long	nb;
+	int		i;
+
+	nb = n;
+	if (nb == 0)
+		return (ft_strdup("0"));
+	i = ft_len(nb);
+	str = (char *)malloc(sizeof(char) * (i + 1));
+	if (!str)
+		return (NULL);
+	str[i--] = '\0';
+	if (nb < 0)
+	{
+		str[0] = '-';
+		nb *= -1;
+	}
+	while (nb > 0)
+	{
+		str[i] = '0' + (nb % 10);
+		nb = nb / 10;
+		i--;
+	}
 	return (str);
 }
