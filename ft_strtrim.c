9d2a56a84f92ae85977d87d38e5a3db574419f2b
FARAHALHAYAT <94931048+FARAHALHAYAT@users.noreply.github.com>
2022-08-02 18:38:24 +0400

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,53 +12,18 @@
 
 #include "libft.h"
 
-int	ft_getstart(const char *s1, const char *set)
-{
-	size_t	len;
-	size_t	i;
-
-	len = ft_strlen(s1);
-	i = 0;
-	while (i < len)
-	{
-		if (ft_strchr(set, s1[i]) == 0)
-			break ;
-		i++;
-	}
-	return (i);
-}
-
-int	ft_getend(const char *s1, const char *set)
-{
-	size_t	len;
-	size_t	i;
-
-	len = ft_strlen(s1);
-	i = 0;
-	while (i < len)
-	{
-		if (ft_strchr(set, s1[len - i - 1]) == 0)
-			break ;
-		i++;
-	}
-	return (len - i);
-}
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	int		start;
-	int		end;
-	char	*newstr;
+	size_t		len;
+	char		*tab;
 
 	if (!s1 || !set)
 		return (NULL);
-	start = ft_getstart((char *)s1, (char *)set);
-	end = ft_getend(char *)s1, (char *)set);
-	if (start >= end)
-		return (ft_strdup(""));
-	newstr = (char *)malloc(sizeof(char) * (end - start + 1) + 1);
-	if (!newstr)
-		return (NULL);
-	ft_strlcpy(newstr, s1 + start, end - start + 1);
-	return (newstr);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	len = ft_strlen(s1);
+	while (len && ft_strchr(set, s1[len]))
+		len--;
+	tab = ft_substr((char *)s1, 0, len + 1);
+	return (tab);
 }
