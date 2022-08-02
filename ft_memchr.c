ef05d7327aac775e9dba4be0b2fd059cb91a0dec
FARAHALHAYAT <94931048+FARAHALHAYAT@users.noreply.github.com>
2022-08-03 02:46:33 +0400

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -14,13 +14,14 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	const unsigned char	*p;
+	size_t i;
 
-	p = (unsigned const char *)s;
-	while (n--)
+	i = 0;
+	while (i < n)
 	{
-		if (*p++ == (unsigned char)c)
-			return ((void *)(p - 1));
+		if (((unsigned char*)s)[i] == (unsigned char)c)
+			return ((void*)(s + i));
+		i++;
 	}
-	return (NULL);
+	return (0);
 }
