35b01eae90fe4e03cfa4796f646febeb652269ef
FARAHALHAYAT <94931048+FARAHALHAYAT@users.noreply.github.com>
2022-08-03 20:51:36 +0400

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -18,11 +18,9 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		i;
 
 	i = 0;
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
 	str = (char *)malloc(sizeof(char *) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (str == NULL)
-		return (NULL);
+	if (!s1 || !s || !str)
+		return (0);
 	while (*s1 != '\0')
 	{
 		str[i] = *s1++;
