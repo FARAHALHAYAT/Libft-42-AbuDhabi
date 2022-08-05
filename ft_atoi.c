b1b3c00ab0ea416d0511dbdcfecfdbe1fec1d376
FARAHALHAYAT <94931048+FARAHALHAYAT@users.noreply.github.com>
2022-08-05 23:04:01 +0400

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -102,3 +102,38 @@ int ft_atoi(const char *str)
     }
     return (res * minus);
 }
+
+
+
+#include"libft.h"
+
+const char	*skip_whitespace(const char *str)
+{
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	return (str);
+}
+
+int	ft_atoi(const char	*str)
+{
+	long	n;
+	long	sign;
+
+	n = 0;
+	sign = 1;
+	str = skip_whitespace(str);
+	if (*str == '-')
+		sign = -1;
+	if ((*str == '-') || (*str == '+'))
+		str++;
+	while ((*str != '\0') && ('0' <= *str) && (*str <= '9'))
+	{
+		n = (n * 10) + (*str - '0');
+		if (n > 2147483647 && sign == 1)
+			return (-1);
+		if (n > 2147483648 && sign == -1)
+			return (0);
+		str++;
+	}
+	return (n * sign);
+}
