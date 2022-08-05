00e9dd8f381c1a385aa54c6a1833453ff792c557
FARAHALHAYAT <94931048+FARAHALHAYAT@users.noreply.github.com>
2022-08-05 21:52:19 +0400

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -14,23 +14,28 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	unsigned char		*dp;
-	const unsigned char	*sp;
+    size_t  i;
+    int     pos;
 
-	dp = (unsigned char *)dst;
-	sp = (unsigned char *)src;
-	if (!dst && !src)
-		return (0);
-	if (dst < src)
-	{
-		ft_memcpy(dst, src, len);
-	}
-	else if (dp > sp)
-	{
-		while (len--)
-		{
-			dp[len] = sp[len];
-		}
-	}
-	return (dst);
+		i = 0;
+    if (src == NULL && dst == NULL)
+			return (dst);
+    if (dst < src)
+    {
+			while (i < len)
+			{
+				((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+				i++;
+			}
+    }
+    else
+    {
+			while (i < len)
+			{
+				pos = len - 1 - i;
+				((unsigned char *)dst)[pos] = ((unsigned char *)src)[pos];
+				i++;
+			}
+    }
+    return (dst);
 }
