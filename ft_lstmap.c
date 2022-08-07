98ded6bbb4b151ad796e796acb5cf4c87921baab
FARAHALHAYAT <94931048+FARAHALHAYAT@users.noreply.github.com>
2022-08-07 13:20:29 +0400

    Update ft_lstmap.c

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -10,6 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+** DESCRIPTION:
+** 		Iterates the list ’lst’ and applies the function ’f’ to the content of
+**	each element. Creates a new list resulting of the successive applications of
+**	the function ’f’. The ’del’ function is used to delete the content of an
+**	element if needed.
+*/
+
 #include "libft.h"
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
