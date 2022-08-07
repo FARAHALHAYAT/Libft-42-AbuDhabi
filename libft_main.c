86bd3822f427524cb758eb8fcd0bb08e2ad382fd
FARAHALHAYAT <94931048+FARAHALHAYAT@users.noreply.github.com>
2022-08-07 22:56:28 +0400

    Update libft_main.c

--- libft_main.c
+++ libft_main.c
@@ -136,3 +136,122 @@ int	main(void)
 		splitted_str[1], splitted_str[2], splitted_str[3]);
 	return (0);
 }
+	main_strchr.c or strrchr
+int	main (void)
+{
+	const char	str[] = "http://www.tutorialspoint.com";
+	const char	ch = '.';
+	char		*ret;
+
+	ret = ft_strchr(str, ch);
+	printf("String after ft_strchr |%c| is - |%s|\n", ch, ret);
+	ret = strchr(str, ch);
+	printf("String after   strchr |%c| is - |%s|\n", ch, ret);
+	return (0);
+}
+	main_strdup.c
+int	main(void)
+{
+	char	*src;
+	char	*dup;
+
+	src = "Farahhhhhhhhhhhhhhhhhhhhhhhh";
+	dup = ft_strdup(src);
+	printf ("src = %s and dup = %s\n", src, dup);
+	return (0);
+}
+	 main_strjoin.c 
+ int main()
+{
+	char *str1 = "let's see";
+	char *str2 = " how this works";
+	printf("%s\n", ft_strjoin(str1, str2));
+}
+	 main_strlcat.c
+int main(void)
+{
+	int	s;
+	int	r;
+
+	s = -1;
+	while (++s <= 13)
+	{
+		char	src[5] = "casa";
+		char	dst[9] = "amor";
+		if (s < 1)
+		{
+			printf("src:%s |dst:%s\n", src, dst);
+			printf("r = strlcat(dst, src, size)\n");
+		}
+		r = strlcat(dst, src, s);
+		printf("size:%d | src:%s | dst:%s | return: %d\n", s, src, dst, r);
+	}
+		 return (0);
+	 }
+		main_strlcpy.c
+int	main(void)
+{
+	int 	src_size;
+	char 	*src;
+	char 	*dest;
+
+	src = ft_calloc(11, sizeof(char));
+	dest = ft_calloc(8, sizeof(char));
+	src = "alo galera";
+	src_size = ft_strlcpy(dest, src, 8);
+	printf("src: %s\n", src);
+	printf("dest: %s\n", dest);
+	printf("src_size: %d\n", src_size);
+	return(0);
+}
+main_strncmp.c
+int	main(void)
+{
+	char	str1[15];
+	char	str2[15];
+	int		ret;
+
+	ft_memcpy(str1, "abcdef", 6);
+	ft_memcpy(str2, "ABCDEF", 6);
+	ret = ft_strncmp(str1, str2, 5);
+	if (ret > 0)
+		printf("str2 is less than str1\n");
+	else if (ret < 0)
+		printf("str1 is less than str2\n");
+	else
+		printf("str1 is equal to str2\n");
+	return (0);
+}
+	 main_strnstr.c 
+int	main(void)
+{
+	char	*str1;
+	char	*str2;
+
+	str1 = "dalibftd\0a";
+	str2 = "libft";
+	printf("My function output: %s\n", ft_strnstr(str1, str2, 12));
+	return (0);
+}
+	 main_strtrim.c  
+ int	main(void)
+{
+	char	*str;
+	char	*set;
+
+	str = "abc affffff abc";
+	set = "abc";
+	printf("%s", ft_strtrim(str, set));
+	return (0);
+}
+  main_substr.c
+int	main(void)
+{
+	char *str;
+	char *substr;
+
+	str = "topa tudo por";
+	substr = ft_substr(str, 1, 10);
+	printf ("%s should be a substr of %s", substr, str);
+	return (0);
+}
