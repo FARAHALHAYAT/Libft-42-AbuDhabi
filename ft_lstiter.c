b2e5bd3b09ba2a52bc65d46cf51ddb6865e031ca
FARAHALHAYAT <94931048+FARAHALHAYAT@users.noreply.github.com>
2022-08-07 13:24:33 +0400

    Update ft_lstiter.c

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -10,6 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+** DESCRIPTION:
+** 		Iterates the list ’lst’ and applies the function ’f’ to the content of
+**	each element.
+*/
+
 #include "libft.h"
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
